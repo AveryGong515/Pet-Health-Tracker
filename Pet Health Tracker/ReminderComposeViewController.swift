6fae047c5c33fc1917405d96e252427142306314
AveryGong515 <lilygong515@gmail.com>
2024-04-21 17:34:03 -0400

    added select pet feature in reminder compose

--- Pet Health Tracker/ReminderComposeViewController.swift
+++ Pet Health Tracker/ReminderComposeViewController.swift
@@ -10,15 +10,50 @@ import UIKit
 class ReminderComposeViewController: UIViewController {
 
     
-    @IBOutlet weak var petName: UIButton!
+    
+    
+
+    
+    
+    @IBOutlet weak var petNameSelectionButton: UIButton!
+    @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var reminderTitle: UITextField!
+    var petChosen: Pet?
     override func viewDidLoad() {
         super.viewDidLoad()
+        descriptionTextView.layer.cornerRadius = 10
+        
+        petNameSelectionButton.addAction(UIAction(title: "", handler: {(_) in print("default action")}), for: .touchUpInside)
+        petNameSelectionButton.menu = setPetNameMenuOptions()
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
     
+   
 
+    
+    func setPetNameMenuOptions()->UIMenu{
+        var petNameOptionsActions = [String]()
+        for pet in Pet.getPets(){
+            petNameOptionsActions.append(pet.name)
+        }
+        var actions = [UIAction]()
+        for petName in petNameOptionsActions{
+            actions.append(UIAction(title: petName, handler: choosePet))
+        }
+        return UIMenu(title: "Choose Pet Name", options:[.displayInline,.singleSelection], children: actions)
+        
+        
+        
+    }
+    
+    func choosePet(action: UIAction){
+        petNameSelectionButton.setTitle(action.title, for: .normal)
+        
+    }
     
      @IBAction func pickDateTime(_ sender: Any) {
          
